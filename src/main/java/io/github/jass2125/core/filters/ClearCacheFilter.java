74a03fea7e8f39672c31bc4fd71851923122c201
Anderson Souza <jair_anderson_bs@hotmail.com>
2018-03-19 22:01:46 -0300

    add clear cache filter

---
+++ src/main/java/io/github/jass2125/core/filters/ClearCacheFilter.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.github.jass2125.core.filters;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Anderson Souza <jair_anderson_bs@hotmail.com>
+ * @since Mar 19, 2018 9:51:26 PM
+ */
+//@WebFilter(filterName = "", urlPatterns = {"", ""})
+public class ClearCacheFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        httpResponse.setHeader("Expires", "0");
+        httpResponse.setHeader("Cache-Control", "no-store, no-cache, must-revalidate");
+        httpResponse.addHeader("Cache-Control", "post-check=0, pre-check=0");
+        httpResponse.setHeader("Pragma", "no-cache");
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
