b67210fe27ca273e74d2f28fced19db69a799822
Anderson Souza <jair_anderson_bs@hotmail.com>
2018-03-19 21:47:50 -0300

    add User services

---
+++ src/main/java/io/github/jass2125/core/services/client/UserServiceImp.java
@@ -0,0 +1,17 @@
+/**
+ *
+ */
+package io.github.jass2125.core.services.client;
+
+import io.github.jass2125.core.services.imp.UserService;
+import javax.ejb.Stateless;
+
+
+/**
+ * @author Anderson Souza
+ *
+ */
+@Stateless
+public class UserServiceImp implements UserService {
+
+}
---
+++ src/main/java/io/github/jass2125/core/services/imp/UserService.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package io.github.jass2125.core.services.imp;
+
+/**
+ * @author Anderson Souza
+ *
+ */
+public interface UserService {
+
+}
