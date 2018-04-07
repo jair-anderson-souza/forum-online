cbe15b0e9b64b618504ea620d397bd7414bbfcb5
Anderson Souza <jair_anderson_bs@hotmail.com>
2018-04-07 01:54:29 -0300

    add dao

---
+++ src/main/java/io/github/jass2125/forum/core/dao/client/UserPrincipalDao.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.github.jass2125.forum.core.dao.client;
+
+import io.github.jass2125.forum.core.entity.UserPrincipal;
+
+/**
+ * @author Anderson Souza <jair_anderson_bs@hotmail.com>
+ * @since Apr 6, 2018 11:56:04 PM
+ */
+public interface UserPrincipalDao {
+
+    public UserPrincipal searchByEmailAndPassword(UserPrincipal user);
+}
