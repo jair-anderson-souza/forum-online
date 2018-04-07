a7fbd949826e76d88f4f928fd669b515e84d2d2c
Anderson Souza <jair_anderson_bs@hotmail.com>
2018-04-07 03:59:49 -0300

    add exceptions

---
+++ src/main/java/io/github/jass2125/forum/core/exceptions/EncodingException.java
@@ -0,0 +1,20 @@
+package io.github.jass2125.forum.core.exceptions;
+
+
+import java.io.UnsupportedEncodingException;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * @author Anderson Souza <jair_anderson_bs@hotmail.com>
+ * @since Apr 6, 2018 11:12:59 PM
+ */
+public class EncodingException extends RuntimeException {
+
+    public EncodingException(UnsupportedEncodingException e, String msg) {
+    }
+
+}
---
+++ src/main/java/io/github/jass2125/forum/core/exceptions/UserNotFoundExcetion.java
@@ -0,0 +1,20 @@
+package io.github.jass2125.forum.core.exceptions;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+/**
+ * @author Anderson Souza <jair_anderson_bs@hotmail.com>
+ * @since Apr 7, 2018 1:44:58 AM 
+ */
+public class UserNotFoundExcetion extends RuntimeException {
+
+    public UserNotFoundExcetion(String msg, Exception e) {
+    }
+
+}
