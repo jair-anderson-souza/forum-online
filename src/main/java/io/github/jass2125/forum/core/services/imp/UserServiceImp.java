6b0261ec81d3ad94d966cc4952d63842309b36b8
Anderson Souza <jair_anderson_bs@hotmail.com>
2018-04-07 02:09:29 -0300

    add services

--- src/main/java/io/github/jass2125/forum/core/services/imp/UserServiceImp.java
+++ src/main/java/io/github/jass2125/forum/core/services/imp/UserServiceImp.java
@@ -5,6 +5,8 @@ package io.github.jass2125.forum.core.services.imp;
 
 import io.github.jass2125.forum.core.dao.client.UserPrincipalDao;
 import io.github.jass2125.forum.core.entity.UserPrincipal;
+import io.github.jass2125.forum.core.exceptions.CryptographyException;
+import io.github.jass2125.forum.core.exceptions.EncodingException;
 import io.github.jass2125.forum.core.exceptions.UserNotFound;
 import io.github.jass2125.forum.core.services.client.UserService;
 import javax.ejb.EJB;
@@ -25,11 +27,23 @@ public class UserServiceImp implements UserService {
 
     @Override
     public UserPrincipal login(UserPrincipal user) {
-        String encryptPassword = cryptography.encryptPassword(user.getPassword());
+        String encryptPassword = encryptPassword(user.getPassword());
         user.setPassword(encryptPassword);
+        return findUser(user);
+    }
+
+    public UserPrincipal findUser(UserPrincipal user) {
         try {
             return userPrincipalDao.searchByEmailAndPassword(user);
         } catch (UserNotFound e) {
+            throw e;
+        }
+    }
+
+    public String encryptPassword(String password) {
+        try {
+            return cryptography.encryptPassword(password);
+        } catch (CryptographyException | EncodingException e) {
             throw e;
         }
     }
