9779ef234e2fc15b4325fd3ecda9b3453deb7340
Anderson Souza <jair_anderson_bs@hotmail.com>
2018-04-09 01:46:16 -0300

    add controller topic

---
+++ src/main/java/io/github/jass2125/forum/core/controllers/TopicController.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.github.jass2125.forum.core.controllers;
+
+import io.github.jass2125.forum.core.entity.Topic;
+import java.io.Serializable;
+import javax.ejb.EJB;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import io.github.jass2125.forum.core.services.client.TopicService;
+
+/**
+ *
+ * @author Anderson Souza <jair_anderson_bs@hotmail.com>
+ */
+@Named
+@RequestScoped
+public class TopicController implements Serializable {
+
+    @Inject
+    private Topic topic;
+    @EJB
+    private TopicService topicService;
+
+    public void persist() {
+
+    }
+
+}
