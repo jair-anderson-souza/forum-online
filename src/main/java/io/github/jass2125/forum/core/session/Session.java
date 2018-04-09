4d56864739d3de2559581b3c1d9cd2a1266b20cb
Anderson Souza <jair_anderson_bs@hotmail.com>
2018-04-09 01:43:58 -0300

    rename class

--- src/main/java/io/github/jass2125/forum/core/session/Session.java
+++ src/main/java/io/github/jass2125/forum/core/session/Session.java
@@ -2,8 +2,8 @@ package io.github.jass2125.forum.core.session;
 
 import io.github.jass2125.forum.core.entity.UserPrincipal;
 import java.io.Serializable;
-import java.util.Map;
 import javax.inject.Inject;
+import redis.clients.jedis.Jedis;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -14,12 +14,16 @@ import javax.inject.Inject;
  * @author Anderson Souza <jair_anderson_bs@hotmail.com>
  * @since Apr 7, 2018 5:10:33 PM
  */
-public class FacesSession implements Serializable {
+public class Session implements Serializable {
 
     @Inject
-    private Map<String, Object> session;
+    private Jedis jedis;
 
     public void addUserToSession(UserPrincipal user) {
-        session.put("user", user);
+        jedis.set("user", String.valueOf(user.getId()));
+    }
+
+    public String getUserFromSession() {
+        return jedis.get("user");
     }
 }
