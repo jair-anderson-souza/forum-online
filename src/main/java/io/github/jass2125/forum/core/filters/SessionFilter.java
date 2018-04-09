1b32f8b6ecc8b0a1977ab2bec9a37781a6cb7a5e
Anderson Souza <jair_anderson_bs@hotmail.com>
2018-04-09 01:45:24 -0300

    edit filter session

--- src/main/java/io/github/jass2125/forum/core/filters/SessionFilter.java
+++ src/main/java/io/github/jass2125/forum/core/filters/SessionFilter.java
@@ -5,8 +5,8 @@
  */
 package io.github.jass2125.forum.core.filters;
 
-import io.github.jass2125.forum.core.entity.UserPrincipal;
 import java.io.IOException;
+import javax.inject.Inject;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -16,7 +16,7 @@ import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import redis.clients.jedis.Jedis;
 
 /**
  * @author Anderson Souza <jair_anderson_bs@hotmail.com>
@@ -25,6 +25,9 @@ import javax.servlet.http.HttpSession;
 @WebFilter(filterName = "sessionFilter", urlPatterns = "/user/*")
 public class SessionFilter implements Filter {
 
+    @Inject
+    private Jedis jedis;
+
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
     }
@@ -32,10 +35,9 @@ public class SessionFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest req = ((HttpServletRequest) request);
-        HttpSession session = req.getSession();
         HttpServletResponse resp = ((HttpServletResponse) response);
         String contextPath = req.getContextPath();
-        UserPrincipal user = (UserPrincipal) session.getAttribute("user");
+        String user = jedis.get("user");
         if (user == null) {
             resp.sendRedirect(contextPath + "/signin.xhtml");
             chain.doFilter(request, response);
